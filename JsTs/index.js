40937d9c91cc85a3943caf39ce07ecb3c2a16a72
Cyborg3Ninja <taohanzeng.gmail.com>
2026-07-12 23:07:24 +0800

    rework structure, start of java script

---
+++ JsTs/index.js
@@ -0,0 +1,14 @@
+console.log(`Hello World!`);
+console.log(`i like pizza!`);
+//-> inspect element -> console
+// window.alert(`This is an alert!`);
+// window.alert(`I like pizza`);
+// This is a comment, it will not be read by the computer. It is for humans to read.
+/*
+This is a multi-line comment. 
+It will not be read by the computer. 
+It is for humans to read.
+*/
+document.getElementById(`myH1`).textContent = `Hello World!`;
+document.getElementById(`myP`).textContent = `I like pizza`;
+// inject text to html
